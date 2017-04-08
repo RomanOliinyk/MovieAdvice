e2511648078d313568c55f6e02cb36425da09c48
RomanOliinyk <roman.oliinyk.ua@gmail.com>
2017-04-08 19:47:27 +0300

    testing years

--- recommendation/views.py
+++ recommendation/views.py
@@ -23,18 +23,32 @@ class MovieListView(generic.ListView):
                 pk__in=checked_genres or [])
         return self._filtered_genres
 
+    #@property
+    #def filtered_years(self):
+        #if not hasattr(self, '_filtered_years'):
+            #years_list = self.request.GET.get('year1')
+        #print (years_list)
+        #return years_list
+
+
     def get_queryset(self):
         query = super(MovieListView, self).get_queryset()
-        if self.filtered_genres :
+        #if self.filtered_years:
+            #years = self.filtered_years
+            #print (years)
+        if self.filtered_genres:
             genres = self.filtered_genres
             for genre in genres:
                 query = query.filter(genres=genre)
+
         return query.order_by('-vote_count')
 
     def get_context_data(self, **kwargs):
         context = super(MovieListView, self).get_context_data(**kwargs)
         context['genres'] = Genre.objects.all()
         context['filtered_genres'] = self.filtered_genres
+        years = Movie.objects.all().dates('release_date', 'year')
+        context['years_list'] = years
         return context
 
 # Movie detail page
