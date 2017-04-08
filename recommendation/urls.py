5c0248f8441ba84cce9c710499386406336c3aa7
RomanOliinyk <roman.oliinyk.ua@gmail.com>
2017-04-08 19:02:49 +0300

    little non-influencal changes

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'recommendation'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^movie/$', views.MovieListView.as_view(), name='movie-list'),
-    url(r'^movie/(?P<pk>[0-9]+)/$', views.MovieDetailView.as_view(),
-        name='movie-detail'),
-    url(r'^person/$', views.PersonListView.as_view(), name='person-list'),
+    url(r'^movie/$', views.MovieListView.as_view(), name='movie_list'),
+    url(r'^movie/(?P<pk>\d+)/', views.MovieDetailView.as_view(),
+        name='movie_detail'),
+    url(r'^person/$', views.PersonListView.as_view(), name='person_list'),
     url(r'^person/(?P<pk>[0-9]+)/$', views.PersonDetailView.as_view(),
-        name='person-detail'),
+        name='person_detail'),
 ]
