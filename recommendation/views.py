85a7df627c8960235012edf5212cc7c4d5fa626a
RomanOliinyk <roman.oliinyk.ua@gmail.com>
2017-04-10 14:42:23 +0300

    Added seach by year, need to add default

--- recommendation/views.py
+++ recommendation/views.py
@@ -17,18 +17,25 @@ class MovieListView(generic.ListView):
 
     @property
     def filtered_genres(self):
-        if not hasattr(self, '_filtered_genres'):
+        if not hasattr(self, '_filtered_genre'):
             checked_genres = self.request.GET.getlist('genre')
             self._filtered_genres = Genre.objects.filter(
                 pk__in=checked_genres or [])
         return self._filtered_genres
 
+    @property
+    def filtered_years(self):
+        if not hasattr(self, '_filtered_years'):
+            checked_years = self.request.GET.get('year')
+        return checked_years
+
 
     def get_queryset(self):
         query = super(MovieListView, self).get_queryset()
-        #if self.filtered_years:
-            #years = self.filtered_years
-            #print (years)
+        if self.filtered_years:
+            years = self.filtered_years
+            if years != 'any':
+                query = query.filter(release_date__year = years)
         if self.filtered_genres:
             genres = self.filtered_genres
             for genre in genres:
@@ -40,8 +47,8 @@ class MovieListView(generic.ListView):
         context = super(MovieListView, self).get_context_data(**kwargs)
         context['genres'] = Genre.objects.all()
         context['filtered_genres'] = self.filtered_genres
-        #years = Movie.objects.all().dates('release_date', 'year')
-        #context['years_list'] = years
+        years = Movie.objects.all().dates('release_date', 'year')
+        context['years'] = years
 
         # query_url for pagination without ('page=N')
         query_url = self.request.GET.urlencode()
